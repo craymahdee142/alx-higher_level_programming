256e9d51224044a010aed32527f60c39c7b50a17
craymahdee142 <craymahdaniel@gmail.com>
2023-06-07 05:08:37 +0000

    vi 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/urs/bin/python3
+def uppercase(str):
+    for i in range (len(str)):
+        char = ord(str[i])
+        if char >= 97 and char <= 122:
+            char = char - 32
+        print("{}".format(char(char)), end"")
+    print()
