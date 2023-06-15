600fda3226dc17126967fcbce473d2d4ef00cbe6
craymahdee142 <craymahdaniel@gmail.com>
2023-06-15 01:25:48 +0000

    more data structures, set, dict

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    num = 0
+    result = 0
+    for score, weight in my_list:
+        num += score * weight
+        result += weight
+    weighted_average = num / result
+    return (weighted_average)
