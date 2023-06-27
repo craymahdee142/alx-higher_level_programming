07ed24f2fbe3d57ce0a871816e3bd5c7056442df
craymahdee142 <craymahdaniel@gmail.com>
2023-06-27 00:41:41 +0000

    try/exception

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > 0:
+                raise Exception("Too far")
+            else:
+                result += a**b / i
+        except Exception:
+            result = b + a
+            break
+        return (result)
