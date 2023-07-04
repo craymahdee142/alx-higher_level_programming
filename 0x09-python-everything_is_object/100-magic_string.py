32bf82013a8905a3fd98c8d36004412cc10b1eb4
craymahdee142 <craymahdaniel@gmail.com>
2023-07-04 21:40:09 +0000

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(static={"count": 0}):
-    static["count"] += 1
-    return str("BestSchool ", * static["count"])[:-2]
+def magic_string():
+    magic_string.a = getattr(magic_string, 'a', 0) + 1
+    return ("BestSchool, " * (magic_string.a - 1)) + "BestSchool"
