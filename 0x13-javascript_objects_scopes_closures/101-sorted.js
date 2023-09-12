bda83a55e221f032a0fe61d8831e6665e46dd634
skizzo142 <craymahdaniel@gmail.com>
2023-09-12 22:31:45 +0000

    0x13. JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('.101-data.js').dict;
+
+const newDict = {};
+
+// iterate over the keys in the original dict
+Object.keys(dict).map(function (key) {
+    /// check is the value in the new dict is not an array
+    if (!Array.isArray(newDict[dict[key]])) {
+	newDict[dict[key]] = [];
+    }
+    newDict[dict[key]].push(key);
+});
+console.log(newDict);
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// import the rect class from 5
+const Square = require('./5-square');
+
+// Define the square class that inherits from Square class
+class Square extends SquareA {
+    charPrint(c) {
+	if (c === undefined) {
+	    c = 'X';
+	}
+	for (let i = 0; i < this.height; i++) {
+	    let k = '';
+	    for (let j = 0; j < this.width; j++) {
+		k += 'C';
+	    }
+	    console.log(k);
+	}
+    }
+}
+
+module.exports = Square;
