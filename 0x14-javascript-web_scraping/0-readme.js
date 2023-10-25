ba780cc3fefff1f1c5bffc42e666fd44ea85a488
craymahdee142 <craymahdaniel@gmail.com>
2023-10-24 19:51:06 -0700

    updated task 0 & 3

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-// Get the path from command line
-const filePath = process.argv[2];
-// Read the file in utf-8
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,13 @@ const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (error, body) {
-  console.log(error || JSON.parse(body).title);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if {response.statusCode === 200) {
+    const responseJSON = JSON.parse(bdy);
+    console.log(responseJSON.title);
+ } else {
+  console.log('Error code: ' + response.statusCode);
+ }
 });
