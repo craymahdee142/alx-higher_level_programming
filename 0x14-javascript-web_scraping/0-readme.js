e662853b4052247e73365e1b6f9c5b259901ad13
craymahdee142 <craymahdaniel@gmail.com>
2023-10-24 17:25:03 -0700

    vi 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the path from command line
+const filePath = process.argv[2];
+// Read the file in utf-8
+fs.readFile(filePath, 'ut-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
